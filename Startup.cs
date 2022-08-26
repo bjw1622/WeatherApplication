77bf85e3686bb4238ad0aa0475aa84f565e26a17
JaeWon Baek <bjw1622@gmail.com>
2022-08-26 14:54:41 +0900

    실시간 채팅 기능 추가

---
+++ Startup.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNet.SignalR;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WeatherApplication
+{
+    public class Startup : Hub
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
